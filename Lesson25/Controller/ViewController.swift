f6b55550c8428ecaaadea6bede32f33eaa4abd57
Valery <ihValery@gmail.com>
2021-04-01 18:59:01 +0300

    Первые тесты для структуры Создание Запись Дата

--- Lesson25/Controller/ViewController.swift
+++ Lesson25/Controller/ViewController.swift
@@ -2,15 +2,5 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    private(set) var volume = 0
-    
-    func setVolume(value: Int)
-    {
-        volume = min(max(value, 0), 100)
-    }
-    
-    func chacarterCompare(strOne: String, strTwo: String) -> Bool
-    {
-        return Set(strOne) == Set(strTwo)
-    }
+
 }
---
+++ Lesson25Tests/Model/TaskTests.swift
@@ -0,0 +1,36 @@
+import XCTest
+@testable import Lesson25
+
+class TaskTests: XCTestCase
+{
+    //Создание задачи при инициализации с заголовком
+    func testInitTaskWithTitle()
+    {
+        let task = Task(title: "Foo")
+        XCTAssertNotNil(task, "Задача не создалась")
+    }
+    
+    func testInitTaskWithTitleAndDescription()
+    {
+        let task = Task(title: "Foo", description: "Bar")
+        XCTAssertNotNil(task)
+    }
+    
+    func testWhenGivenTitleSetsTitle()
+    {
+        let task = Task(title: "Foo")
+        XCTAssertEqual(task.title, "Foo", "Задача не создалась")
+    }
+    
+    func testWhenGivenDescriptionSetsDescription()
+    {
+        let task = Task(title: "Foo", description: "Bar")
+        XCTAssertTrue(task.description == "Bar", "Описание не создалось")
+    }
+    
+    func testTaskInitWithDate()
+    {
+        let task = Task(title: "Foo")
+        XCTAssertNotNil(task.date, "У задачи нет даты")
+    }
+}
