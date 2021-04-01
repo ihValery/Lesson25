6aa72dc69f1853ccb1a09b3081cd76f34206b86c
Valery <ihValery@gmail.com>
2021-04-01 14:36:57 +0300

    Start

--- Lesson25/System32/SceneDelegate.swift
+++ Lesson25/System32/SceneDelegate.swift
@@ -1,17 +1,9 @@
-//
-//  SceneDelegate.swift
-//  Lesson25
-//
-//  Created by Валерий Игнатьев on 1.04.21.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -46,7 +38,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
