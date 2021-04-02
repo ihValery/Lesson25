079867f6ce7a16bef55c21d61a68e1c2c7474844
Valery <ihValery@gmail.com>
2021-04-03 00:45:38 +0300

    21. CheckTitleLabelInCell. Проверка на Находиться ли titleLabel ячейки внутри cell.contentView (isDescendant) + LocationLabel

---
+++ Lesson25Tests/View/TaskCellTests.swift
@@ -0,0 +1,52 @@
+import XCTest
+@testable import Lesson25
+
+class TaskCellTests: XCTestCase {
+
+    var cell: TaskCell!
+    
+    override func setUpWithError() throws {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(identifier: "TaskListViewController") as! TaskListViewController
+        controller.loadViewIfNeeded()
+        
+        let tableView = controller.tableView
+        let dataSource = FakeDataSource()
+        tableView?.dataSource = dataSource
+        
+        cell = tableView?.dequeueReusableCell(withIdentifier: "taskCell", for: IndexPath(row: 0, section: 0)) as? TaskCell
+    }
+
+    override func tearDownWithError() throws {
+        cell = nil
+        super.tearDown()
+    }
+    
+    func testCellHasTitleLabel() {
+        XCTAssertNotNil(cell.titleLabel)
+    }
+    
+    func testCellHasTitleLabelInContentView() {
+        //Находиться ли titleLabel ячейки внутри cell.contentView
+        XCTAssertTrue(cell.titleLabel.isDescendant(of: cell.contentView))
+    }
+    
+    func testCellHasLocationLabelInContentView() {
+        XCTAssertTrue(cell.locationLabel.isDescendant(of: cell.contentView))
+    }
+}
+
+extension TaskCellTests {
+    
+    class FakeDataSource: NSObject, UITableViewDataSource {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+    }
+}
